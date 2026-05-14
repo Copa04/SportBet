7041f7afbf839f7e8326b8f15f4d3a1d6c12d456
andrei-maxim16 <cezar-andrei.maxim@student.tuiasi.ro>
2026-05-14 22:40:06 +0300

    services

--- SportBet.Services/DataService.cs
+++ SportBet.Services/DataService.cs
@@ -1,19 +1,20 @@
 // Autor: Echipa SportBet
-// Functionalitate: Serviciu central pentru persistenta datelor in fisiere JSON locale.
-//                  Gestioneaza incarcarea si salvarea datelor mock, precum si
-//                  initializarea repository-urilor la pornirea aplicatiei.
-//
+// Functionalitate: Serviciu central care incarca datele din fisiere JSON locale
+//                  si initializeaza repository-urile la pornirea aplicatiei.
+
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
 using SportBet.Models;
 using SportBet.Repositories;
 
 namespace SportBet.Services
 {
     /// <summary>
-    /// Serviciu central de date care gestioneaza persistenta JSON si
-    /// ofera acces la repository-urile aplicatiei.
-    /// Utilizeaza fisiere JSON locale ca sursa de date (mock data).
+    /// Serviciu central de date care citeste fisierele JSON si
+    /// populeaza repository-urile la pornirea aplicatiei.
     /// </summary>
     public class DataService
     {
@@ -24,17 +25,6 @@ namespace SportBet.Services
 
         /// <summary>Calea catre fisierul JSON cu meciurile.</summary>
         private const string FISIER_MECIURI = "Data/meciuri.json";
-
-        /// <summary>Calea catre fisierul JSON cu tichetele.</summary>
-        private const string FISIER_TICHETE = "Data/tichete.json";
-
-        #endregion
-
-        #region Campuri private
-
-        private List<Utilizator> _utilizatori;
-        private List<Meci> _meciuri;
-        private List<Tichet> _tichete;
 
         #endregion
 
@@ -51,160 +41,73 @@ namespace SportBet.Services
 
         #endregion
 
-        #region Constructori
+        #region Constructor
 
         /// <summary>
-        /// Constructor implicit. Nu incarca datele automat.
-        /// Apelati <see cref="Initializeaza"/> dupa instantiere.
+        /// Constructor implicit.
         /// </summary>
         public DataService()
         {
-            _utilizatori = new List<Utilizator>();
-            _meciuri = new List<Meci>();
-            _tichete = new List<Tichet>();
         }
 
         #endregion
 
-        #region Metode publice – Initializare
+        #region Metode publice
 
         /// <summary>
-        /// Initializeaza serviciul: incarca datele din JSON si construieste repository-urile.
-        /// Daca fisierele JSON nu exista, populeaza cu date mock initiale.
+        /// Initializeaza serviciul: citeste JSON-urile si populeaza repository-urile.
         /// </summary>
         public void Initializeaza()
         {
-            throw new NotImplementedException();
+            UtilizatorRepository utilizatorRepo = new UtilizatorRepository();
+            MeciRepository meciRepo = new MeciRepository();
+            TichetRepository tichetRepo = new TichetRepository();
+
+            foreach (Utilizator u in IncarcaUtilizatori())
+                utilizatorRepo.Add(u);
+
+            foreach (Meci m in IncarcaMeciuri())
+                meciRepo.Add(m);
+
+            UtilizatorRepository = utilizatorRepo;
+            MeciRepository = meciRepo;
+            TichetRepository = tichetRepo;
+
+            AuthService.Instanta.Initialize(UtilizatorRepository);
         }
-
-        #endregion
-
-        #region Metode publice – Citire JSON
 
         /// <summary>
         /// Incarca lista de utilizatori din fisierul JSON local.
         /// </summary>
-        /// <returns>Lista cu utilizatorii deserializati sau lista goala la eroare.</returns>
+        /// <returns>Lista cu utilizatorii sau lista goala la eroare.</returns>
         public List<Utilizator> IncarcaUtilizatori()
         {
-            throw new NotImplementedException();
+            try
+            {
+                string json = File.ReadAllText(FISIER_UTILIZATORI, Encoding.UTF8);
+                return JsonConvert.DeserializeObject<List<Utilizator>>(json);
+            }
+            catch (Exception)
+            {
+                return new List<Utilizator>();
+            }
         }
 
         /// <summary>
         /// Incarca lista de meciuri din fisierul JSON local.
         /// </summary>
-        /// <returns>Lista cu meciurile deserializate sau lista goala la eroare.</returns>
+        /// <returns>Lista cu meciurile sau lista goala la eroare.</returns>
         public List<Meci> IncarcaMeciuri()
         {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Incarca lista de tichete din fisierul JSON local.
-        /// </summary>
-        /// <returns>Lista cu tichetele deserializate sau lista goala la eroare.</returns>
-        public List<Tichet> IncarcaTichete()
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-
-        #region Metode publice – Scriere JSON
-
-        /// <summary>
-        /// Salveaza lista de utilizatori in fisierul JSON local.
-        /// </summary>
-        /// <param name="utilizatori">Lista de utilizatori de serializat.</param>
-        /// <returns>True daca salvarea a reusit, altfel false.</returns>
-        public bool SalveazaUtilizatori(List<Utilizator> utilizatori)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Salveaza lista de meciuri in fisierul JSON local.
-        /// </summary>
-        /// <param name="meciuri">Lista de meciuri de serializat.</param>
-        /// <returns>True daca salvarea a reusit, altfel false.</returns>
-        public bool SalveazaMeciuri(List<Meci> meciuri)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Salveaza lista de tichete in fisierul JSON local.
-        /// </summary>
-        /// <param name="tichete">Lista de tichete de serializat.</param>
-        /// <returns>True daca salvarea a reusit, altfel false.</returns>
-        public bool SalveazaTichete(List<Tichet> tichete)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Salveaza toate datele (utilizatori, meciuri, tichete) in fisierele JSON.
-        /// </summary>
-        /// <returns>True daca toate salvarile au reusit, altfel false.</returns>
-        public bool SalveazaToateDatale()
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-
-        #region Metode publice – Date mock initiale
-
-        /// <summary>
-        /// Genereaza si salveaza un set de date mock pentru utilizatori (date de test).
-        /// Apelat automat daca fisierul JSON nu exista.
-        /// </summary>
-        public void GenereazaUtilizatoriMock()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Genereaza si salveaza un set de date mock pentru meciuri (date de test).
-        /// Apelat automat daca fisierul JSON nu exista.
-        /// </summary>
-        public void GenereazaMeciuriMock()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Verifica daca directorul de date exista si il creeaza daca lipseste.
-        /// </summary>
-        public void AsiguraDirectorDate()
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
-
-        #region Metode private helper
-
-        /// <summary>
-        /// Serialzeaza un obiect generic in format JSON cu indentare.
-        /// </summary>
-        /// <typeparam name="T">Tipul obiectului de serializat.</typeparam>
-        /// <param name="obiect">Obiectul de serializat.</param>
-        /// <returns>String-ul JSON corespunzator.</returns>
-        private string SerializeazaJson<T>(T obiect)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Deserializeaza un string JSON intr-un obiect de tipul specificat.
-        /// </summary>
-        /// <typeparam name="T">Tipul obiectului de deserializat.</typeparam>
-        /// <param name="json">String-ul JSON.</param>
-        /// <returns>Obiectul deserializat sau default(T) la eroare.</returns>
-        private T DeserializeazaJson<T>(string json)
-        {
-            throw new NotImplementedException();
+            try
+            {
+                string json = File.ReadAllText(FISIER_MECIURI, Encoding.UTF8);
+                return JsonConvert.DeserializeObject<List<Meci>>(json);
+            }
+            catch (Exception)
+            {
+                return new List<Meci>();
+            }
         }
 
         #endregion
